3b6e7f80bae9ddd22ae1b9637211be06469f89b7
remitosbodegon <celuguss02@gmail.com>
2026-09-11 04:46:35 -0300

    Update remitos_app.py

--- remitos_app.py
+++ remitos_app.py
@@ -69,7 +69,7 @@ if "remito_generado" not in st.session_state:
 # ──────────────────────────────────────────────────
 # ESTILO
 # ──────────────────────────────────────────────────
-st.set_page_config(page_title=TITULO_APP, page_icon="🍽️", layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(page_title=TITULO_APP, page_icon="", layout="wide", initial_sidebar_state="collapsed")
 
 # ✅ TU LOGO COMO ÍCONO
 st.markdown("""
